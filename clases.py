2940fca69d54376987fd9f26a09aaf1eb50bdcc0
Yair858 <202411211@urp.edu.pe>
2024-09-05 15:07:24 -0500

    Update clases.py

--- clases.py
+++ clases.py
@@ -1,1 +1,47 @@
+"""
+Clases a usar en nuestros proyectos
+Fecha: 02/09/2024
+Ultima modificación: 03/07/2024
+"""
+class Persona:
+    #def __init__(self,nombre, peso):
+    #    self.nombre = nombre
+    #    self.peso = peso
+    def __init__(self, *args):
+        if len(args) == 0:
+            self.nombre = "Sin nombre"
+            self.peso = 0
+        else:
+            self.nombre = args[0]
+            self.peso = args[1]
 
+    def caminar(self, *args):
+        if len(args) > 0:
+            self.peso -= args[0] / 8.0
+        else:
+            self.peso -= 0.5
+    def comer(self):
+        self.peso +=1
+    def __str__(self):
+        return 'Persona (nombre={}, peso={})'.format(
+            self.nombre, self.peso
+        )
+
+"""
+Nueva clase: Atleta
+"""    
+class Atleta(Persona):
+    def __init__(self, *args):
+        super().__init__(args[0], args[1])
+        if len(args) == 3:          
+            self.estatura = args[2]
+        else:
+            self.estatura = 0.0
+       
+
+    def calcular_imc(self):       
+        return self.peso / (self.estatura * self.estatura)
+    def __str__(self): # Permite obtener una cadena con todos los valores
+        return 'Atleta (nombre={}, peso={}, estatura={})'.format(
+            self.nombre, self.peso, self.estatura
+        )
